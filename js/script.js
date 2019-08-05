00f48259ef68ff5a42fd47f97d29845bdac7a23b
yaroslavvalenty <31990617+yaroslavvalenty@users.noreply.github.com>
2019-08-05 13:29:11 +0300

    New page(completed)

--- js/script.js
+++ js/script.js
@@ -21,3 +21,18 @@ window.addEventListener("scroll", function () {
     }
 });
 
+
+const span = document.querySelector("span");
+
+span.onclick = function() {
+  document.execCommand("copy");
+}
+
+span.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", span.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
+
